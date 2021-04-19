deb9c7620728f09bb64284852c5e83ff6129c7da
Joel Jagarath <joey.jagarath05@gmail.com>
2021-04-19 21:13:07 +0200

    My first commit

---
+++ ShootingStars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShootingStars.Startup))]
+namespace ShootingStars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
