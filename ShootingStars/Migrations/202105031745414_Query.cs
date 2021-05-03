631f329d74664f3a24bd476802dcb220df1cadce
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2021-05-03 19:48:17 +0200

    Fixed Database

---
+++ ShootingStars/Migrations/202105031745414_Query.cs
@@ -0,0 +1,16 @@
+namespace ShootingStars.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Query : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
